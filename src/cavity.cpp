d1c7081cde29192c4e554a5a5923fd3da657842a
Israel Bonilla <ib0574@princeton.edu>
2022-10-26 15:52:38 -0400

    added several function prototypes and initialized flow quantity matrices

--- src/cavity.cpp
+++ src/cavity.cpp
@@ -1,30 +1,77 @@
 #include <iostream>
 #include <fstream>
-#include <eigen3/Eigen/Dense>
+#include <cmath>
 
 #include "cavity.hpp"
 
+#define NGHOST 2
+#define PI 100000 // 1 bar, initial pressure
+#define TI 300 // K, initial temp
+
+using Eigen::MatrixXd;
+
 int main(int argc, char* argv[]) {  
+
+  struct flowParams flow;
+  struct integParams integ;
+  struct flowQuant U;
+
+  double tFinal = 1; // just a guess
+  double L = 1; // TODO: MAKE THIS VARIABLE FOR FUTURE VARIATIONS IN MACH NUMBER
+
+  // set up flow parameters
+  flow.re = 100;    // Reynolds
+  flow.ma = 0.25;   // Mach
+  flow.pr = 0.7;    // Prandtl
+  flow.gamma = 1.4; // cp/cv
+  flow.nu = 0.1;    // kinematic viscosity
+  flow.R = 287;     // gas const
+  flow.uw = 1;
+
+
+  // set up integrator parameters
+  integ.nx = 3;
+  integ.ny = 3;
+  integ.ngx = integ.nx + NGHOST;
+  integ.ngy = integ.ny + NGHOST;
+
+  // caluculate initial density and total energy
+  double rho_i = PI/(flow.R*TI);
+  double et_i = flow.R*TI/(flow.gamma-1);
+
+  // initialize flow quantity matrices
+  U.rho = MatrixXd::Constant(integ.ngx,integ.ngy,rho_i);
+  U.x_mom = MatrixXd::Zero(integ.ngx,integ.ngy);    // lid is stationary at start
+  U.y_mom = MatrixXd::Zero(integ.ngx,integ.ngy);
+  U.et = MatrixXd::Constant(integ.ngx,integ.ngy,et_i);
+
+  //init_quants(flow,integ,U);
+
+  std::cout << U.et << std::endl;
 
   return 0;
 }
 
+int rho_step(struct integParams integ, struct flowQuant U) {
+  // given the integrator parameters and required quantities, take
+  // one timestep of the momentum equation
 
-/*
+  
 
-Gameplan:
+  return 0;
+}
 
-solve:
+int stress(struct flowParams flow, struct integParams integ, struct flowQuant q, int i, int j) {
 
-using r as stand-in for density
+  return 0;
+}
 
-dr/dt + d(ru_i)/dx_i = 0    continuity
+double pressure(struct flowParams flow, const int rho, const int et, const int u, const int v) {
+  /* calculate pressure given a density and total energy via EOS
+  p=rhoRT
+  */
 
-d(ru_i)/dt + d(ru_iu_j)/dx_j = d(sigma_ij)/dx_j   momentum
+  double mag = 0.5*(u*u+v*v);
 
-d(re_t)/dt + d(ru_je_t)/dx_j = d(sigma_ij u_i)/dx_j + d/dx_j(l dT/dx_j)   total energy
-
-p = rRT
-e = 1/(1-g) 
-
-*/
+  return rho*(et-mag)/(flow.gamma-1);
+}
--- src/cavity.hpp
+++ src/cavity.hpp
@@ -1,15 +1,40 @@
 #ifndef CAVITY_HPP_
 #define CAVITY_HPP_
 
-struct Params {
-  int nx;
-  int ny;
-  int ng;
-  int nsteps;
-  double re;
+#include <eigen3/Eigen/Dense>
+
+struct flowParams {
+  int re;
+  double ma;
+  double pr;
+  double gamma;
   double nu;
+  double R;
+  double uw;
 };
 
-int main(int argc, char* argv[]);
+struct integParams {
+  int nx;
+  int ny;
+  int ngx;
+  int ngy;
+  int nt;
+  double dx;
+  double dy;
+  double dt;
+};
+
+struct flowQuant {
+  Eigen::MatrixXd rho;
+  Eigen::MatrixXd x_mom;
+  Eigen::MatrixXd y_mom;
+  Eigen::MatrixXd et;
+};
+
+int rho_step(struct integParams integ, struct flowQuant U);
+
+int stress(struct flowParams flow, struct integParams integ, struct flowQuant U, int i, int j);
+
+double pressure(struct flowParams flow, const int rho, const int et, const int u, const int v);
 
 #endif // CAVITY_HPP_
